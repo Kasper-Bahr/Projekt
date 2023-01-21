7833a371442e9b9d3d7455539e053dd6c0e9af0f
Kasper-Bahr <kasperbahr@wp.pl>
2023-01-21 03:14:40 +0100

    Projekt 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int numProject = random.nextInt(10) + 5;
+
+        Project[] projects = new Project[numProject]
+
+
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("wybierz poziom trudności projektu:");
@@ -13,9 +22,15 @@ public class Main {
         if (choice == 1)
         {
             System.out.println("wybrano łatwy projekt");
+            Project easyProject = new Project("Strona internetowa", 1, 1, 1, 0, 1, 0,"Firma z oknami", "01.04.2020", 500, 3000, 14,"łatwy");
+            easyProject.viewProjectDetails();
         } else if (choice == 2) {
             System.out.println("wybrano średni projekt");
+            Project mediumProject = new Project("prosta gra dla dzieci", 1, 1, 0, 0, 0, 0,"Przedszkole", "10.06.2020", 500, 1500, 30,"średni");
+            mediumProject.viewProjectDetails();
         } else if (choice == 3) {
+            Project hardProject = new Project("aplikacja na telefon", 1, 1, 1, 1, 1, 1,"Firma RTVAGD", "10.06.2021", 8000, 30000, 30,"trudny");
+            hardProject.viewProjectDetails();
             System.out.println("wybrano trudny projekt");
         }else
         {
--- src/Project.java
+++ src/Project.java
@@ -1,7 +1,7 @@
 public class Project {
 
   private String projectName;
-  private int frontEnd, BackEnd, database, mobile, wordPress, prestaShop;
+  private int frontEnd, backEnd, database, mobile, wordPress, prestaShop;
   private String client;
   private String deadline;
   private int penalty;
@@ -12,7 +12,7 @@ public class Project {
     public Project(String projectName, int frontEnd, int backEnd, int database, int mobile, int wordPress, int prestaShop, String client, String deadline, int penalty, int price, int paymentPeriod, String complexity) {
         this.projectName = projectName;
         this.frontEnd = frontEnd;
-        BackEnd = backEnd;
+        this.backEnd = backEnd;
         this.database = database;
         this.mobile = mobile;
         this.wordPress = wordPress;
@@ -26,18 +26,71 @@ public class Project {
     }
     public void viewProjectDetails(){
         System.out.println("Nazwa projektu: " + projectName);
-        System.out.println(" " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
-        System.out.println("Nazwa projektu: " + projectName);
+        System.out.println("Ilość dni roboczych");
+        System.out.println("Front-End" + frontEnd);
+        System.out.println("Back-End " + backEnd);
+        System.out.println("Database " + database);
+        System.out.println("Mobile" + mobile);
+        System.out.println("WordPress" + wordPress);
+        System.out.println("prestaShop" + prestaShop);
+        System.out.println("Klient" + client);
+        System.out.println("Termin oddania: " + deadline);
+        System.out.println("Wyskość kara za przekorczenia terminu" + penalty);
+        System.out.println("Cena jaką klient zapłaci za realizacje projektu" + price);
+        System.out.println("Termin płatności: " + paymentPeriod);
+        System.out.println("Poziom złożoności " + complexity);
 
     }
 
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public int getFrontEnd() {
+        return frontEnd;
+    }
+
+    public int getBackEnd() {
+        return backEnd;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public int getWordPress() {
+        return wordPress;
+    }
+
+    public int getPrestaShop() {
+        return prestaShop;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public int getPenalty() {
+        return penalty;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPaymentPeriod() {
+        return paymentPeriod;
+    }
+
+    public String getComplexity() {
+        return complexity;
+    }
 }
